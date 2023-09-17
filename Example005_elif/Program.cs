4aa999c185eebada7d19aba68fa0b4cbdb7fb22f
Maksim Glukhikh <qwer-87@mail.ru>
2023-09-17 11:47:32 +0500

    Add elif

---
+++ Example005_elif/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "валера")
+{
+    Console.WriteLine("Ебать ты Валера :D");
+}
+else
+{
+    Console.Write("Привет, " + username + "! Ты не Валера!");
+}
