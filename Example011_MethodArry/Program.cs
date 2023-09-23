16f65e6426647a61805dfe406e57de4216066fa4
Maksim Glukhikh <qwer-87@mail.ru>
2023-09-23 15:00:00 +0500

    Добавлен пример работы 11

---
+++ Example011_MethodArry/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {21,24,38,44,25,96,17,38,29};
+int n = array.Length;
+int find = 44;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    
+    index++;
+}
