495e44453be5ccf60a929516bafa0b4d6ac2d746
Maksim Glukhikh <qwer-87@mail.ru>
2023-10-10 10:05:28 +0500

    Добавлены примеры по 4-ой лекции

---
+++ Example013_PrintArray/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+//метод для заполнения двумерного массива
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 100);
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];
+
+FillArray(matrix);
+PrintArray(matrix);
